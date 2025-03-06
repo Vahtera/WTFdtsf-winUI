4e6d2ef8429c505e541179ee3f19e8b8aea4c4cb
Anna Vahtera <anna@vahtera.org>
2025-03-06 16:44:56 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WTFdtsf_winUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox2 = new GroupBox();
+            label3 = new Label();
+            lblAcronymDisplay = new Label();
+            lblTurnDisplay = new Label();
+            btnSubmit = new Button();
+            txtPlayerInput = new TextBox();
+            gboxResults = new GroupBox();
+            listBoxResults = new ListBox();
+            btnExit = new Button();
+            btnNewTurn = new Button();
+            btnEndTurn = new Button();
+            btnReroll = new Button();
+            groupBox2.SuspendLayout();
+            gboxResults.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(lblAcronymDisplay);
+            groupBox2.Controls.Add(lblTurnDisplay);
+            groupBox2.Controls.Add(btnSubmit);
+            groupBox2.Controls.Add(txtPlayerInput);
+            groupBox2.Location = new Point(12, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 144);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(6, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Acronym:";
+            // 
+            // lblAcronymDisplay
+            // 
+            lblAcronymDisplay.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAcronymDisplay.Location = new Point(43, 19);
+            lblAcronymDisplay.Name = "lblAcronymDisplay";
+            lblAcronymDisplay.Size = new Size(687, 65);
+            lblAcronymDisplay.TabIndex = 0;
+            lblAcronymDisplay.Text = "A B C D E F ThisIsALongWord";
+            lblAcronymDisplay.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblTurnDisplay
+            // 
+            lblTurnDisplay.AutoSize = true;
+            lblTurnDisplay.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTurnDisplay.Location = new Point(622, 110);
+            lblTurnDisplay.Name = "lblTurnDisplay";
+            lblTurnDisplay.Size = new Size(71, 21);
+            lblTurnDisplay.TabIndex = 4;
+            lblTurnDisplay.Text = "Player 1";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(695, 110);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(75, 23);
+            btnSubmit.TabIndex = 3;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // txtPlayerInput
+            // 
+            txtPlayerInput.Location = new Point(88, 110);
+            txtPlayerInput.Name = "txtPlayerInput";
+            txtPlayerInput.Size = new Size(524, 23);
+            txtPlayerInput.TabIndex = 2;
+            // 
+            // gboxResults
+            // 
+            gboxResults.Controls.Add(listBoxResults);
+            gboxResults.Location = new Point(12, 191);
+            gboxResults.Name = "gboxResults";
+            gboxResults.Size = new Size(776, 263);
+            gboxResults.TabIndex = 4;
+            gboxResults.TabStop = false;
+            gboxResults.Text = "Results";
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBoxResults.FormattingEnabled = true;
+            listBoxResults.ItemHeight = 32;
+            listBoxResults.Location = new Point(6, 22);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(764, 228);
+            listBoxResults.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(707, 162);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Quit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // btnNewTurn
+            // 
+            btnNewTurn.Location = new Point(626, 162);
+            btnNewTurn.Name = "btnNewTurn";
+            btnNewTurn.Size = new Size(75, 23);
+            btnNewTurn.TabIndex = 6;
+            btnNewTurn.Text = "Play Again";
+            btnNewTurn.UseVisualStyleBackColor = true;
+            btnNewTurn.Click += btnNewTurn_Click;
+            // 
+            // btnEndTurn
+            // 
+            btnEndTurn.Location = new Point(18, 162);
+            btnEndTurn.Name = "btnEndTurn";
+            btnEndTurn.Size = new Size(148, 23);
+            btnEndTurn.TabIndex = 7;
+            btnEndTurn.Text = "End Turn (Show Results)";
+            btnEndTurn.UseVisualStyleBackColor = true;
+            btnEndTurn.Click += btnEndTurn_Click;
+            // 
+            // btnReroll
+            // 
+            btnReroll.Location = new Point(545, 162);
+            btnReroll.Name = "btnReroll";
+            btnReroll.Size = new Size(75, 23);
+            btnReroll.TabIndex = 8;
+            btnReroll.Text = "Reroll";
+            btnReroll.UseVisualStyleBackColor = true;
+            btnReroll.Click += bteReroll_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(793, 461);
+            Controls.Add(btnReroll);
+            Controls.Add(btnEndTurn);
+            Controls.Add(btnNewTurn);
+            Controls.Add(btnExit);
+            Controls.Add(gboxResults);
+            Controls.Add(groupBox2);
+            Name = "Form1";
+            Text = "WTF Does That Stand For?";
+            Load += Form1_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            gboxResults.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBox2;
+        private Label lblTurnDisplay;
+        private Button btnSubmit;
+        private TextBox txtPlayerInput;
+        private Label lblAcronymDisplay;
+        private GroupBox gboxResults;
+        private Label label3;
+        private Button btnExit;
+        private Button btnNewTurn;
+        private Button btnEndTurn;
+        private ListBox listBoxResults;
+        private Button btnReroll;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WTFdtsf_winUI
+{
+    public partial class Form1 : Form
+    {
+        List<string> lstResults = new List<string>();
+        List<string> asChar = new List<string>("ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray().Select(c => c.ToString()).ToList());
+        int numPlayer = 1;
+        int asLength;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            listBoxResults.Items.Clear();
+            lblTurnDisplay.Text = "Player " + numPlayer.ToString();
+            btnNewTurn.Enabled = false;
+
+            asLength = WeightedRandomLength();
+            int asLocation = random.Next(0, asLength);
+            int isWord = random.Next(0, 2);
+
+            Randomize();
+            lblAcronymDisplay.Text = SetAcronymDisplay(asLength);
+        }
+
+        private static int WeightedRandomLength()
+        {
+            Random random = new Random();
+            // Define weights for each length from 2 to 7
+            Dictionary<int, int> weights = new Dictionary<int, int>
+            {
+                { 2, 5 },
+                { 3, 55 },
+                { 4, 30 },
+                { 5, 5 },
+                { 6, 3 },
+                { 7, 2 }
+            };
+
+            // Calculate the total weight
+            int totalWeight = weights.Values.Sum();
+
+            // Generate a random number between 0 and totalWeight - 1
+            int randomNumber = random.Next(totalWeight);
+
+            // Determine the length based on the random number and weights
+            int cumulativeWeight = 0;
+            foreach (var kvp in weights)
+            {
+                cumulativeWeight += kvp.Value;
+                if (randomNumber < cumulativeWeight)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            // Fallback in case of an error
+            return 2;
+        }
+
+        private string SetAcronymDisplay(int nymLength)
+        {
+            Random random = new Random();
+            string tAcronymDisplay = string.Empty;
+            for (int i = 0; i < nymLength; i++)
+            {
+                tAcronymDisplay += asChar[random.Next(asChar.Count)] + " ";
+            }
+            return tAcronymDisplay.Trim();
+        }
+
+        private void Randomize()
+        {
+            Random random = new Random();
+            asLength = WeightedRandomLength();
+            int asLocation = random.Next(0, asLength);
+            int isWord = random.Next(0, 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Size = new Size(814, 230);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnEndTurn_Click(object sender, EventArgs e)
+        {
+            Size = new Size(814, 500);
+            btnSubmit.Enabled = false;
+            txtPlayerInput.Enabled = false;
+            lblTurnDisplay.Text = "---";
+            btnEndTurn.Enabled = false;
+            btnNewTurn.Enabled = true;
+
+            ListBox.ObjectCollection list = listBoxResults.Items;
+            Random random = new Random();
+            int n = list.Count;
+            listBoxResults.BeginUpdate();
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                object value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            listBoxResults.EndUpdate();
+            listBoxResults.Invalidate();
+        }
+
+        private void btnNewTurn_Click(object sender, EventArgs e)
+        {
+            Size = new Size(814, 230);
+            numPlayer = 1;
+            lblTurnDisplay.Text = "Player " + numPlayer.ToString();
+            btnSubmit.Enabled = true;
+            txtPlayerInput.Enabled = true;
+            listBoxResults.Items.Clear();
+            btnEndTurn.Enabled = true;
+            btnNewTurn.Enabled = false;
+            btnReroll.Enabled = true;
+            Randomize();
+            lblAcronymDisplay.Text = SetAcronymDisplay(asLength);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            listBoxResults.Items.Add(txtPlayerInput.Text);
+            txtPlayerInput.Text = string.Empty;
+            numPlayer++;
+            lblTurnDisplay.Text = "Player " + numPlayer.ToString();
+            btnReroll.Enabled = false;
+        }
+
+        private void bteReroll_Click(object sender, EventArgs e)
+        {
+            Randomize();
+            lblAcronymDisplay.Text = SetAcronymDisplay(asLength);
+        }
+    }
+}
